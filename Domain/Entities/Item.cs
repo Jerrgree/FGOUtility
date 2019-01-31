c7d121f81d6d9bd17cfe1cdf3c2d04aa82799e46
Jerrgree <jeremyrgreenburg@gmail.com>
2019-01-30 19:05:03 -0600

    Added Entity Domain Project

---
+++ Domain/Entities/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public int Goal_Id { get; set; }
+        public Goal Goal { get; set; }
+    }
+}
